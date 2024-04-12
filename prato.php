d1031549a7fe9edb14249e91a4f79cf2ef4196da
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2024-04-12 10:36:02 -0300

    Update prato.php

--- prato.php
+++ prato.php
@@ -1,14 +1,12 @@
-<?php include 'header.php'; ?>
+/** @noinspection ALL */<?php include 'header.php'; ?>
 
 <div class="product-page small-11 large-12 columns no-padding small-centered">
+
     <div class="global-page-container">
+
         <?php
-        $cod_prato = $_GET['prato'] ?? ''; // Verifica se o parâmetro 'prato' foi passado na URL
-        $prato_nome = ''; // Inicializa as variáveis
-        $prato_categoria = '';
-        $prato_descr = '';
-        $prato_preco = '';
-        $prato_calorias = '';
+
+        $cod_prato = $_GET['prato'];
 
         $server = 'localhost';
         $user = 'root';
@@ -16,30 +14,34 @@
         $db_name = 'bd_harpos';
         $port = '3307';
 
-        $db_connect = new mysqli($server, $user, $password, $db_name, $port);
-        mysqli_set_charset($db_connect, "utf8");
+        $db_connect = new mysqli($server,$user,$password,$db_name,$port);
+        mysqli_set_charset($db_connect,"utf8");
 
         if ($db_connect->connect_error) {
             echo 'Falha: ' . $db_connect->connect_error;
         } else {
+
             $sql = "SELECT * FROM cardapio WHERE codigo='$cod_prato'";
             $result = $db_connect->query($sql);
 
-            if ($result !== false && $result->num_rows > 0) {
+            if ($result->num_rows > 0) {
+
                 while ($row = $result->fetch_assoc()) {
+
                     $prato_nome = $row['nome'];
                     $prato_categoria = $row['categoria'];
-                    $prato_descr = $row['descricao'];
+                    $prato_descr = $row['descr'];
                     $prato_preco = $row['preco'];
                     $prato_calorias = $row['calorias'];
                 }
-            } else {
-                echo 'Prato não encontrado!' . '<br>';
+
             }
         }
+
         ?>
 
-        <?php if (!empty($prato_nome)) { ?>
+        <?php if ($prato_nome !== NULL) { ?>
+
             <div class="product-section">
                 <div class="product-info small-12 large-5 columns no-padding">
                     <h3><?php echo $prato_nome; ?></h3>
@@ -55,12 +57,17 @@
                 </div>
 
             </div>
-        <?php } ?>
+        <?php } else {
+            echo 'Prato não encontrado!' . '<br>';
+        } ?>
 
         <div class="go-back small-12 columns no-padding">
             <a href="cardapio.php">Voltar ao Cardápio</a>
         </div>
+
     </div>
 </div>
 
 <?php include 'footer.php'; ?>
+
+      
