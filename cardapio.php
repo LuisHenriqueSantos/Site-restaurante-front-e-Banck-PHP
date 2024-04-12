d5f705869c5f8818e387deb71d0ce405017bb0a6
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2024-04-12 11:07:50 -0300

    Update cardapio.php

--- cardapio.php
+++ cardapio.php
@@ -1,75 +1,82 @@
     <?php include 'header.php'; ?>
-           
-        <div class="produto-list small-11 large-12 columns no-padding small-centered">
-            
+
+
+        <div class="cardapio small-11 large-12 columns no-padding small-centered">
             <div class="global-page-container">
                 <div class="produtos-title small-12 columns no-padding">
                     <h3>Cardápio</h3>
                     <hr></hr>
                 </div>
-                
-                <?php
 
-                    $server = 'localhost';
-                    $user = 'root';
-                    $password = '';
-                    $db_name = 'bd_harpos';
-                    $port = '3307';
+                <div class="global-page-container">
 
-                    $db_connect = new mysqli($server,$user,$password,$db_name,$port);
-                    mysqli_set_charset($db_connect,"utf8");
+                    <?php
 
-                    if ($db_connect->connect_error) {
-                        echo 'Falha: ' . $db_connect->connect_error;
-                    } else {
-                        
-                       $sql =  "SELECT DISTINCT categoria FROM restaurante";
+                        $server = 'localhost';
+                        $user = 'root';
+                        $password = '';
+                        $db_name = 'bd_harpos';
+                        $port = '3307';
 
-                        $result = $db_connect->query($sql);
+                        $db_connect = new mysqli($server,$user,$password,$db_name,$port);
+                        mysqli_set_charset($db_connect,"utf8");
 
-                        if ($result->num_rows > 0) { // Verifica o número de linhas retornadas
-                            while ($row = $result->fetch_assoc()) {
-                                $categoria = $row['categoria'];
-                                ?>
-                                <div class="category-slider small-12 columns no-padding">
-                                    <h4><?php echo $categoria; ?></h4>
-                                    <div class="slider-cardapio">
-                                        <div class="slider-002 small-12 small-centered columns">
-                                            <?php
-                                            $sql2 = "SELECT * FROM restaurante WHERE categoria='$categoria'";
-                                            $result2 = $db_connect->query($sql2);
+                        if ($db_connect->connect_error) {
+                            echo 'Falha: ' . $db_connect->connect_error;
+                        } else {
 
-                                            if ($result2->num_rows > 0) {
-                                                while ($row2 = $result2->fetch_assoc()) { ?>
-                                                    <div class="cardapio-item-outer bounce-hover small-10 medium-4 columns">
-                                                        <div class="cardapio-item">
-                                                            <a href="prato.php?prato=<?php echo $row2['codigo']; ?>">
+                           $sql =  "SELECT DISTINCT categoria FROM restaurante";
 
-                                                                <div class="item-image">
-                                                                    <img src="img/cardapio/<?php echo $row2['nomeimagem']; ?>" alt="cogumelos"/>
-                                                                </div>
+                            $result = $db_connect->query($sql);
 
-                                                                <div class="item-info">
-                                                                    <div class="title"><?php echo $row2['nome']; ?></div>
-                                                                </div>
+                            if ($result->num_rows > 0) { // Verifica o número de linhas retornadas
+                                while ($row = $result->fetch_assoc()) {
+                                    $categoria = $row['categoria'];
+                                    ?>
 
-                                                                <div class="gradient-filter">
-                                                                </div>
-                                                            </a>
+                                    <div class="category-slider small-12 columns no-padding">
+                                        <h4><?php echo $categoria; ?></h4>
+
+                                        <div class="slider-cardapio">
+                                            <div class="slider-002 small-12 small-centered columns">
+
+                                                <?php
+                                                $sql2 = "SELECT * FROM restaurante WHERE categoria='$categoria'";
+                                                $result2 = $db_connect->query($sql2);
+
+                                                if ($result2->num_rows > 0) {
+                                                    while ($row2 = $result2->fetch_assoc()) { ?>
+                                                        <div class="cardapio-item-outer bounce-hover small-10 medium-4 columns">
+                                                            <div class="cardapio-item">
+                                                                <a href="prato.php?prato=<?php echo $row2['codigo']; ?>">
+
+                                                                    <div class="cardapio-item-image">
+                                                                        <img src="img/cardapio/<?php echo $row2['nomeimagem']; ?>" alt="cogumelos"/>
+                                                                    </div>
+
+                                                                    <div class="item-info">
+                                                                        <div class="title"><?php echo $row2['nome']; ?></div>
+                                                                    </div>
+
+                                                                    <div class="gradient-filter">
+                                                                    </div>
+                                                                </a>
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                <?php }
-                                            }
-                                            ?>
+                                                    <?php }
+                                                }
+                                                ?>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            <?php }
-                        } else {
-                            echo 'Não há itens';
-                        }    
-                    }
-                ?>
+                                <?php }
+                            } else {
+                                echo 'Não há itens';
+                            }
+                        }
+                    ?>
+
+                </div>
             </div> 
         </div>       
 
